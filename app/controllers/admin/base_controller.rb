4b33fe82ea1dc661eb093bcc291c494bc6136d98
ben nelson <ben.volatic@gmail.com>
2024-12-25 20:29:46 -0800

    about to implement auth for admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,11 @@
 module Admin
-    class BaseController < ApplicationController
-      # Authentication will be added here later
+  class BaseController < ApplicationController
+    before_action :authenticate_admin!
+
+    private
+
+    def authenticate_admin!
+      redirect_to admin_login_path, alert: 'Please log in to access the admin dashboard.' unless admin_signed_in?
     end
   end
-  
+end
